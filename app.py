ce11c7d53971076931398c8e2890e0a75db0a400
indravathimeesala <meesalaindravathi@gmail.com>
2026-07-04 22:31:07 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+# Create uploads folder if not exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 @app.route("/")
 def home():
-    return "<h1>Construction Safety Document Intelligence</h1>"
-if __name__=="__main__":
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files.get("document")
+
+    if file and file.filename != "":
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+        file.save(filepath)
+        return "<h2>File Uploaded Successfully!</h2>"
+
+    return "<h2>No File Selected</h2>"
+
+if __name__ == "__main__":
     app.run(debug=True)
